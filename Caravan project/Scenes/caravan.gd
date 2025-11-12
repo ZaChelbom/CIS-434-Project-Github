b3fed38a20280e8912cdb10205f14cd8b05b037c
ZaChelbom <121643845+ZaChelbom@users.noreply.github.com>
2025-11-11 19:51:59 -0500

    feature: basic caravan functionality
    Implemented a (jank) way for Caravans to store cards just like the hand.

--- Caravan project/Scenes/caravan.gd
+++ Caravan project/Scenes/caravan.gd
@@ -1,2 +1,98 @@
+class_name Caravan
 extends Node2D
 
+var owned_by: String
+
+var caravan_value: int# the sum value of the caravan
+var is_sold: bool
+var caravan_suit: String # clubs, diamonds, hearts, spades
+var caravan_direction: String # ascending, descending
+var is_outbidding: bool
+
+@export var caravan_curve: Curve
+@export var rotation_curve: Curve
+
+@export var max_rotation_degrees := 5
+@export var y_sep := -10 # seperation between cards on the y axis in pixels, negative values mean the cards will cram together, positive values mean space in between
+@export var x_min := 0 # offset on the x-axis compared to the caravan's position
+@export var x_max := -15 # maximum amount of x offset that can be applied to cards based on the hand card
+
+const CARD_SCENE_PATH="res://Scenes/card.tscn" #temp
+
+
+
+
+
+func _ready() -> void:
+	caravan_value = 0
+	_update_caravan_properties()
+	pass # Replace with function body.
+
+
+func add_card_to_caravan():
+	var card_scene = preload(CARD_SCENE_PATH)
+	var new_card = card_scene.instantiate()
+	$tract.add_child(new_card)
+	_update_cards()
+
+
+func _update_cards():
+	var cards := $tract.get_child_count()
+	var all_cards_size := Card.SIZE.y * cards + y_sep * (cards-1)
+	var final_y_sep := y_sep
+
+	if all_cards_size > $tract.size.y:
+		final_y_sep = ($tract.size.y - Card.SIZE.y * cards) / (cards -1)
+		all_cards_size = $tract.size.y
+
+	var offset = ($tract.size.y - all_cards_size) / 2
+
+	for i in cards:
+		var card := $tract.get_child(i)
+		var x_multiplier := caravan_curve.sample(1.0/ (cards-1) * i)
+		var rot_multiplier := rotation_curve.sample(1.0 / (cards-1) *i)
+
+		if cards == 1:
+			x_multiplier = 0.0
+			rot_multiplier = 0.0
+
+		var final_x: float = x_min + x_max * x_multiplier
+		var final_y: float = offset + Card.SIZE.y * i + final_y_sep * i
+
+		card.position = Vector2(final_x,final_y)
+		card.rotation_degrees = max_rotation_degrees * rot_multiplier
+
+
+
+
+
+func _update_caravan_properties():
+	if caravan_value > 20 and caravan_value < 27:
+		$value_panel/value_text_label.text = "[color=black]%d" %[caravan_value]
+		is_sold = true
+	else:
+		$value_panel/value_text_label.text = "[color=red]%d" %[caravan_value]
+		is_sold = false
+	
+	match caravan_suit:
+		"clubs": 
+			$suit_panel/suit_text_label.text = "[color=black]♣"
+		"diamonds":
+			$suit_panel/suit_text_label.text = "[color=red]♦"
+		"hearts":
+			$suit_panel/suit_text_label.text = "[color=red]♥︎"
+		"spades":
+			$suit_panel/suit_text_label.text = "[color=black]♠︎"
+		_: # if none of the patterns match
+			$suit_panel/suit_text_label.text = ""
+
+	match caravan_direction:
+		"ascending":
+			$direction_panel/direction_text_label.text = "↑"
+		"descending":
+			$direction_panel/direction_text_label.text = "↓"
+		_: # if none of the patterns match
+			$direction_panel/direction_text_label.text = ""
+	
+	if is_outbidding != null:
+		$outbid_panel/outbid_text_label.visible = is_outbidding
--- Caravan project/Scripts/main_game.gd
+++ Caravan project/Scripts/main_game.gd
@@ -2,6 +2,7 @@ extends Node2D
 
 @onready var hand: Hand = $Hand
 @onready var deck: Deck = $Deck
+const CARAVAN_SCENE_PATH="res://Scenes/caravan.tscn"
 
 func _ready() -> void:
 	get_viewport().physics_object_picking_first_only = true
@@ -10,10 +11,31 @@ func _ready() -> void:
 	for i in 8: # draw 8 cards from deck on startup
 		deck.draw_card()
 
+	for i in 3: # create 3 player caravans
+		var caravan_scene = preload(CARAVAN_SCENE_PATH)
+		var new_caravan = caravan_scene.instantiate()
+		new_caravan.owned_by = "player"
+		new_caravan.name = "player_caravan_%d" %[i]
+		add_child(new_caravan)
+		new_caravan.position = Vector2(i*240,360)
+
+	for i in 3: # create 3 opponent caravans
+		var caravan_scene = preload(CARAVAN_SCENE_PATH)
+		var new_caravan = caravan_scene.instantiate()
+		new_caravan.owned_by = "cpu"
+		new_caravan.name = "cpu_caravan_%d" %[i]
+		add_child(new_caravan)
+		new_caravan.position = Vector2(i*240,-8)
+
 
 func _on_draw_card_button_pressed() -> void:
-	deck.draw_card()
+	var caravan_node = get_node("player_caravan_2")
+	caravan_node.add_card_to_caravan()
 
+func add_to_caravan():
+	var caravan_node = get_node("player_caravan_2")
+	caravan_node.add_card_to_caravan()
+	pass
 
 func _on_discard_card_button_pressed() -> void:
 	hand._discard()
