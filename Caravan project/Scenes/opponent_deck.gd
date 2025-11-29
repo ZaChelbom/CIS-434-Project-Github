fbf798036e4c83352ff4f9df5c5484fc2c931978
ZaChelbom <121643845+ZaChelbom@users.noreply.github.com>
2025-11-29 16:27:28 -0500

    feature: added nodes for CPU
    Added some nodes and prototype scripts that the AI will eventually use.

---
+++ Caravan project/Scenes/opponent_deck.gd
@@ -0,0 +1,73 @@
+class_name opponent_deck
+extends Node2D
+
+const CARD_DATABASE_PATH = "res://Scripts/card_database.gd"
+const CARD_SCENE_PATH="res://Scenes/card.tscn"
+
+var deck = []
+
+var card_suit_array = [
+	"clubs",
+	"diamonds",
+	"hearts",
+	"spades"
+]
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+func load_deck():
+	SaveLoadManager._load()
+	var saved_deck_keys_array = SaveLoadManager.deck_to_save.keys()
+	var card
+	for i in 13:
+		for j in 4:
+			if SaveLoadManager.deck_to_save[saved_deck_keys_array[i]][j] == 0:
+				continue
+			
+			for k in SaveLoadManager.deck_to_save[saved_deck_keys_array[i]][j]:
+				card = str(saved_deck_keys_array[i]) + "_of_" + str(card_suit_array[j])
+				deck.insert(0,card)
+				
+	for l in SaveLoadManager.deck_to_save["joker"][0]: #load the jokers
+		card = "joker"
+		deck.insert(0,card)
+
+	deck.shuffle()
+	_update_deck()
+
+
+func _update_deck():
+	if deck.size() == 0:
+		$DeckImage.visible = false
+	$DeckLabel.text = "Deck: %d" % [deck.size()]
+
+
+func draw_card():
+	if deck.size() == 0:
+		print("Empty deck")
+		return
+	
+	var card_drawn_name = deck[0]
+	deck.erase(card_drawn_name)
+	var card_image_path = "res://Assets/%s.png" %[card_drawn_name]
+	var card_scene = preload(CARD_SCENE_PATH)
+	var card_database = preload(CARD_DATABASE_PATH) # this is dumb and bad I should have scrapped this
+	var new_card: Card = card_scene.instantiate()
+	new_card.get_node("CardIMGfront").texture = load(card_image_path)
+	new_card.is_projection = false
+	new_card.card_name = card_drawn_name
+	new_card.value = card_database.CARDS[card_drawn_name][0]
+	new_card.suit = card_database.CARDS[card_drawn_name][1]
+	new_card.card_type = card_database.CARDS[card_drawn_name][2]
+
+
+	$"../opponent_hand".add_card_to_hand(new_card)
+	_update_deck()
+
+
+# Called every frame. 'delta' is the elapsed time since the prev9ious frame.
+func _process(delta: float) -> void:
+	pass
